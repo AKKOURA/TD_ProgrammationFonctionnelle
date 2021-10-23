2c4e4aa321046b304a063e826cfeb3c77d7d54e4
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2021-10-24 01:22:21 +0200

    suiteQ2

--- TD2/EXO1/Q2/Main.java
+++ TD2/EXO1/Q2/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 public class Main {
 
     public static void main(String[] args){
@@ -11,5 +12,14 @@ public class Main {
     List <String> list=List.of("A9","A1","A2");
     ToString<List<String>> listToString=c->c.stream().reduce("",(String a, String b)->a+","+b);        
     System.out.println(listToString.convertir(list));   
+    // //Lambdas  pour une map
+    Map<String, String>  hmap = new HashMap();
+    hmap.put(1, "e1");
+    hmap.put(2, "e2");
+    hmap.put(3, "e3");  
+	ToString<Map<String, String>> mapToString =map->map.stream().reduce("", (Map<String, String> e)->e.getKey() + " : " + e.getValue());	
+    System.out.println(mapToString.convertir(hmap)); 
+    
+
     } 
 }
