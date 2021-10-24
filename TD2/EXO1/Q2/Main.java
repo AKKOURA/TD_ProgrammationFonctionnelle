93c8e4d529c256f25b991bd5a7f1f86592b1306a
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2021-10-24 17:30:52 +0200

    suite

--- TD2/EXO1/Q2/Main.java
+++ TD2/EXO1/Q2/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.*;
 import java.util.Map;
@@ -10,27 +11,19 @@ public class Main {
 
 
     // //Lambdas sur une liste
-    List <String> list=List.of("A9","A1","A2");
-    ToString<List<String>> listToString=c->c.stream().reduce("",(String a, String b)->a+","+b);        
-    System.out.println(listToString.convertir(list));   
-    // //Lambdas  pour une map
-    // Map hmap = new HashMap();
-    // hmap.put("1", "e1");
-    // hmap.put("2", "e2");
-    // hmap.put("3", "e3");  
-	// ToString<Map<String, String>> mapToString =map->map.stream().reduce("", (Map<String, String> e)->e.getKey() + " : " + e.getValue());
-    // ToString<Map<String,String>> mapToString =hmap.forEach((Map.Entry<String, String> entry)-> entry.getKey() + " : " + entry.getValue() );
-    // hmap.forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+    List <String> listt=List.of("A9","A1","A2");
+    //autre mé
+    // ToString<List<String>> listToString=c->c.stream().reduce("",(String a, String b)->a+","+b);  
+    ToString<List<String>> listToString =(List<String> list)->{for(int i =0 ;i<list.size();i++) System.out.print(list.get(i) +","); return "" ;  };
+    System.out.println(listToString.convertir(listt));   
+ 
     Map<Integer, String> map = new HashMap<Integer, String>();
     map.put(1, "foo");
     map.put(2, "bar");
     map.put(3, "baz");
-    ToString<Map<Integer, String>> result = map.entrySet()
-        .stream()
-        .reduce("", (Map.Entry<Integer,String> e)->e.getKey() + " : " + e.getValue());
-   
 
-    System.out.println(result.convertir(map)); 
+    ToString<Map<Integer, String>> mapToString =(Map<Integer, String> mapp)->{  mapp.forEach((k, v) -> System.out.print( k + " : " + v+ " , ")); return "" ;   };
+    System.out.println(mapToString .convertir(map)); 
     
 
     } 
