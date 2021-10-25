285c943964783544a9c451c2e745776a5f42e449
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2021-10-25 09:47:39 +0200

    Update MainPredicate.java

--- TD2/EXO2/MainPredicate.java
+++ TD2/EXO2/MainPredicate.java
@@ -8,41 +8,33 @@ public class MainPredicate {
     public static void main(String[] args){
 
         //Q1
-        Predicate<Integer> tailleTropPetite = (Integer taille )-> taille <100;
-        Predicate<Integer> tailleTropGrande = (Integer taille )-> taille >200;
-        Predicate<Double> poidsTropLourd = (Double poids  )-> poids >150.0;
+        Predicate<Paire<Integer,Double>> tailleTropPetite = (Paire<Integer,Double> taille )-> taille.fst <100;
+        Predicate<Paire<Integer,Double>> tailleTropGrande = (Paire<Integer,Double> taille )-> taille.fst >200;
+        Predicate<Paire<Integer,Double>> poidsTropLourd = (Paire<Integer,Double> poids  )-> poids.snd >150.0;
 
         // taille incorrecte (par composition) 
-        Predicate <Integer>  inCorrectTaille =tailleTropPetite.or(tailleTropGrande);
+        Predicate <Paire<Integer,Double>>  inCorrectTaille =tailleTropPetite.or(tailleTropGrande);
 
         // taille correcte (par composition) 
-        Predicate <Integer>  correctTaille =inCorrectTaille.negate();
+        Predicate <Paire<Integer,Double>>  correctTaille =inCorrectTaille.negate();
 
        // poids correct (par composition)
-       Predicate<Double> correctPoids = poidsTropLourd.negate();
+       Predicate<Paire<Integer,Double>> correctPoids = poidsTropLourd.negate();
        
         //accès autorisé (par composition)
-        Predicate <Paire<Integer,Double>> predicateCorrect =(Paire<Integer,Double> val)-> val.fst>100 && val.fst>200 && val.snd<150.0 ;
-        System.out.println(predicateCorrect.test(new Paire(150,140.5)));
-        
-
-
-        // test 
-        // Predicate <Paire<Integer,Double>>  predicateCorrect =(Paire<Integer,Double> val)-> val.fst<100   && val.snd>150.0 ;  
-        // System.out.println(predicateCorrect.test(new Paire(1,1.5)));
+        Predicate <Paire<Integer,Double>> predicateCorrect =correctPoids.and(correctTaille);
         
         List<Integer> elemIntegers = Arrays.asList(100,50,300);
-        List<Predicate<Integer>> elemsPredicates =Arrays.asList(tailleTropGrande,tailleTropPetite);
+        List<Predicate<Paire<Integer,Double>>> elemsPredicates =Arrays.asList(tailleTropGrande,tailleTropPetite);
         List<Integer> elemList = filtragePredicatif(elemsPredicates,elemIntegers);
         System.out.println(elemList);   
-    
      } 
      //Q2
-     static <T> List<T> filtragePredicatif(List<Predicate<T>> predicates, List<T> elems){
+     static <T,S,V> List<T> filtragePredicatif(List<Predicate<Paire<S,V>>> predicates, List<T> elems){
         List<T> list = new ArrayList<T>() ;
-        for(int i=0;i<predicates.size();i++){
+        for(Predicate i :predicates){
             for(int j=0;j<elems.size();j++){
-                if(predicates.get(i).test(elems.get(j))==true){
+                if(i.test(elems.get(j))==true){
                     list.add(elems.get(j));
                 }   
             }
